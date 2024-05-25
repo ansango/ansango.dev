7a23f4a404671988c6eaebb754d536eab28e058f
ansango <anibalsantosgo@gmail.com>
2024-05-25 03:07:01 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://dev.ansango.com",
   integrations: [sitemap(), tailwind(), pagefind()],
   markdown: {
     rehypePlugins: [rehypeAstroRelativeMarkdownLinks, [

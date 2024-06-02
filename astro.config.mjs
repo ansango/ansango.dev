1612a0f9699aeef8390ebbf74c5f97b1625cacd2
ansango <anibalsantosgo@gmail.com>
2024-06-02 15:50:26 +0200

    chore: Update package name and Astro version, add partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,21 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
 import markdown from "./src/lib/plugins/markdown"
+import partytown from '@astrojs/partytown'
 // https://astro.build/config
 export default defineConfig({
   site: "https://dev.ansango.com",
-  integrations: [sitemap(), tailwind(), pagefind()],
-  markdown
+  integrations: [
+    sitemap(),
+    tailwind(),
+    pagefind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      }
+    }),
+  ],
+  markdown,
 });
 
 

19a4d13877379aab82b0fd81ba039dd8b3186ed4
ansango <anibalsantosgo@gmail.com>
2025-10-18 10:23:18 +0200

    Add Image component and PlaceholderIcon for bookmarks and reading layouts

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export * from "./theme";
 export { default as Heading } from "./heading.astro";
 export { default as TreeNode } from "./tree-node.astro";
 export { default as TreeNodePlain } from "./tree-node.plain.astro";
+export { default as Image } from "./image.astro";
--- src/components/ui/icons/index.ts
+++ src/components/ui/icons/index.ts
@@ -3,3 +3,4 @@ export { default as TagIcon } from "./tag-icon.astro";
 export { default as ThemeIcon } from "./theme-icon.astro";
 export { default as ExternalLinkIcon } from "./external-link-icon.astro";
 export { default as InfoIcon } from "./info-icon.astro";
+export { default as PlaceholderIcon } from "./placeholder.icon.astro";

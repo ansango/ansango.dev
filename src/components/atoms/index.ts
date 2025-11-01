05758569c93dda90f092ae9fff20d67223c2b9d4
ansango <anibalsantosgo@gmail.com>
2025-11-01 22:14:36 +0100

    refactor(components): extract layout elements and reorganize atoms/molecules
    - Move Head, Header and Footer into src/components/layout/elements and add elements index
    - Remove those exports from atoms index and update default layout import
    - Move Pagination and TreeNode exports into molecules and add new molecule components:
      pagination.astro, tree-node.astro, playnow.svelte, playnow-mini.svelte
    - Update organism import (play-now) to use molecules version
    - Enhance head with ClipboardScript, ClientRouter and SVG restart script

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,13 +2,9 @@ export * from "./image";
 export { default as Link } from "./link.astro";
 export { default as Tag } from "./tag.astro";
 export { default as Svg } from "./svg.astro";
-export { default as Head } from "./head.astro";
-export { default as TreeNode } from "./tree-node.astro";
+
 export { default as ClipboardScript } from "./clipboard.script.astro";
 export { default as Container } from "./container.astro";
 export { default as Divider } from "./divider.astro";
-export { default as Footer } from "./footer.astro";
-export { default as Header } from "./header.astro";
-export { default as Pagination } from "./pagination.astro";
 export * from "./theme";
 export * from "./searcher"
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export * from "./entries";
-
+export { default as Pagination } from "./pagination.astro";
+export { default as TreeNode } from "./tree-node.astro";

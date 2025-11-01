ece190cf83534c0c3e88c83d35d84ce7f767295b
ansango <anibalsantosgo@gmail.com>
2025-11-01 17:33:45 +0100

    refactor(reading): extract reading layout into organisms (hero, list, pagination) and remove LayoutReading

--- src/components/molecules/layout/index.ts
+++ src/components/molecules/layout/index.ts
@@ -2,5 +2,4 @@ export { default as LayoutDefault } from "./default.astro";
 export { default as LayoutSingle } from "./single.astro";
 export { default as LayoutArchive } from "./archive.astro";
 export * from "./collection";
-export { default as LayoutReading } from "./reading.astro";
 export { default as LayoutBookmarks } from "./bookmarks.astro";

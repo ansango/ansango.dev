4e898860b0a9fe12ba6a2347fb42c905c7276f85
ansango <anibalsantosgo@gmail.com>
2025-11-01 20:31:47 +0100

    refactor(collection): remove legacy layout/organisms and wire templates
    - Delete legacy molecules/layout/collection components (collection, collection.default, collection.entry, collection.wiki) and organisms/collection index
    - Stop re-exporting collection layouts from molecules/layout index
    - Remove WikiTemplate export from organisms/wiki index
    - Update templates/collection to import and use WikiTemplate and DefaultTemplate (use DefaultTemplate as fallback) instead of the removed layout components

--- src/components/molecules/layout/collection/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LayoutCollection } from "./collection.astro";
-export { default as LayoutCollectionWiki } from "./collection.wiki.astro";
-export { default as LayoutCollectionDefault } from "./collection.default.astro";
-export { default as LayoutCollectionEntry } from "./collection.entry.astro";
--- src/components/molecules/layout/index.ts
+++ src/components/molecules/layout/index.ts
@@ -1,4 +1,4 @@
 export { default as LayoutDefault } from "./default.astro";
 export { default as LayoutSingle } from "./single.astro";
-export * from "./collection";
+
 

74042fef9f20a7e0e4debcbd0d20337e72780238
ansango <anibalsantosgo@gmail.com>
2025-11-01 20:26:53 +0100

    refactor(collection): extract collection UI into templates & organisms; add entry templates
    - Add new collection templates and organisms (index, default, single, wiki) and entry template
    - Export collection & entry templates from templates index
    - Update pages/[collection]/[...slug].astro to stop reading meta/entriesPerPage directly
    - Temporarily disable ThemeScript, Theme and Searcher in head/footer/header (commented out)
    - Add bun.lock (lockfile) to lock dependencies

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,3 +1,2 @@
-export { default as CollectionIndexTemplate } from "./index.collection.astro";
-export { default as CollectionSinglePageTemplate } from "./single.collection.astro";
-export { default as CollectionEntryTemplate } from "./entry.collection.astro";
+export * from "./collection";
+export * from "./entry"
